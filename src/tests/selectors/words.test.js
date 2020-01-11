42500608130b326baaa58180e7e229821ba52c81
Manuel Schulze <manuel.schulze@plan-b-gmbh.com>
2020-01-11 20:51:37 +0100

    test: Add reducers tests

--- src/tests/selectors/words.test.js
+++ src/tests/selectors/words.test.js
@@ -1,24 +1,5 @@
 import selectWords from '../../selectors/words'; 
-
-const words = [{
-    id: '1',
-    source: 'gut',
-    destination: 'dobro',
-    factor: 1,
-    repeatAt: 0
-}, {
-    id: '2',
-    source: 'bin ich nicht',
-    destination: 'nisam',
-    factor: 1.5,
-    repeatAt: 1024
-}, {
-    id: '3',
-    source: 'danke',
-    destination: 'hvala',
-    factor: 3,
-    repeatAt: -1000
-}];
+import words from '../fixtures/words';
 
 test('should filter by text value with match in destination', () => {
     const filters = {
